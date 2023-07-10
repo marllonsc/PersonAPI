2b2ad08632c42bdcac874a8d4d6e920b077e6f4a
Marllon Costa <marllonsc@gmail.com>
2023-07-10 20:15:33 -0300

    testing fix ip service on mysql

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,38 @@
+echo "Deployment Start >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.!"
+echo ""
 
 mvn clean install
+echo ""
 
 eval $(minikube docker-env)             
+echo ""
 
 minikube docker-env | Invoke-Expression 
+echo ""
 
 docker build -t person:0.0.1 .
+echo ""
 
 kubectl delete pod person
+echo ""
 
 kubectl run person --image=person:0.0.1 --image-pull-policy=Never
+echo ""
 
 # Run on first time only -------------------------------------------------------------------------
 
 #kubectl delete service person  
+echo ""
 
 #kubectl expose pod person --type=NodePort --port=8080 --target-port=8080 --name=person
+echo ""
 
-# End block first time-------------------------------------------------------------------------
+# End block first time----------------------------------------------------------------------------
 
 minikube service person --url
-
+echo ""
+echo ""
+echo ""
 
 # Check the exit code of the deployment command
 if [ $? -eq 0 ]; then
@@ -28,3 +40,5 @@ if [ $? -eq 0 ]; then
 else
     echo "Deployment failed!"
 fi
+
+echo "Deployment End >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.!"
